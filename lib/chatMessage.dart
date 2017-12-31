a04775b1a99def9f1261b7a5b40c39cd5fd1101b
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2017-12-31 16:25:33 +0800

    Wrap longer lines

--- lib/chatMessage.dart
+++ lib/chatMessage.dart
@@ -20,19 +20,21 @@ class ChatMessage extends StatelessWidget {
           child: new Row(
             crossAxisAlignment: CrossAxisAlignment.start, // the parent is a Row widget whose main axis is horizontal, so CrossAxisAlignment.start gives it the highest position along the vertical axis
             children: <Widget>[
-              new Container(
-                margin: const EdgeInsets.only(right: 16.0),
+              new Container( // avatar
+                margin: const EdgeInsets.only(left: 4.0, right: 10.0),
                 child: new CircleAvatar(child: new Text(_senderName[0])),
               ),
-              new Column(
-                crossAxisAlignment: CrossAxisAlignment.start, // For messages, the parent is a Column widget whose main axis is vertical, so CrossAxisAlignment.start aligns the text at the furthest left position along the horizontal axis
-                children: <Widget>[                           // display the sender's name on top and the text of the message below
-                  new Text(_senderName, style: Theme.of(context).textTheme.subhead), // To style the sender's name and make it larger than the message text
-                  new Container(
-                    margin: const EdgeInsets.only(top: 5.0),
-                    child: new Text(messageText),
-                  )
-                ],
+              new Expanded(
+                child: new Column( // username & message
+                  crossAxisAlignment: CrossAxisAlignment.start, // For messages, the parent is a Column widget whose main axis is vertical, so CrossAxisAlignment.start aligns the text at the furthest left position along the horizontal axis
+                  children: <Widget>[                           // display the sender's name on top and the text of the message below
+                    new Text(_senderName, style: Theme.of(context).textTheme.subhead), // To style the sender's name and make it larger than the message text
+                    new Container(
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: new Text(messageText),
+                    )
+                  ],
+                ),
               )
             ],
           ),
